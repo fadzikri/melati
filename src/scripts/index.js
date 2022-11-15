538577a874766e26844ef2d197e31606cd43140a
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-15 21:24:07 +0700

    Simpan

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import '../styles/main.css';
 
 const menu = document.getElementById("hamburger");
 const navbar = document.getElementById("drawer");
+const tahun = document.getElementById("tahun");
 const main = document.querySelector("main");
 
 menu.addEventListener("click", (event) => {
@@ -14,3 +15,7 @@ main.addEventListener("click", (event) => {
     navbar.classList.remove("navbar-open");
     event.stopPropagation();
 })
+
+let year = new Date().getFullYear();
+
+tahun.innerHTML = year;
