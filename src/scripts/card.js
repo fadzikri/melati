bd5e78ed24f67c21a94ee682e49be369297fb2d0
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-16 17:00:35 +0700

    🥶

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -4,10 +4,10 @@ let cards = document.getElementById("cards");
 
 data.restaurants.forEach((x) => {
     cards.innerHTML += `
-        <div class="card">
-            <img src=${x.pictureId} alt="${x.name}" width="100%">
-            <p>Rating : ${x.rating} / 5</p>
+        <div class="card" tabindex="0">
+            <img src=${x.pictureId} alt="" width="100%">
             <p>Nama : ${x.name}</p>
+            <p>Rating : ${x.rating} / 5.0</p>
             <p>Lokasi : Kota ${x.city}</p>
         </div>
     `;
