9fe46346db0bb3b7d74ce0dfa8519dd04043da67
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-16 08:07:51 +0700

    Grid

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -5,11 +5,10 @@ let cards = document.getElementById("cards");
 data.restaurants.forEach((x) => {
     cards.innerHTML += `
         <div class="card">
-            <img src=${x.pictureId} alt="${x.name}">
-            <p>${x.rating}</p>
-            <p>${x.name}</p>
-            <p>${x.city}</p>
-            <p>${x.description}</p>
+            <img src=${x.pictureId} alt="${x.name}" width="100%">
+            <p>Rating : ${x.rating} / 5</p>
+            <p>Nama : ${x.name}</p>
+            <p>Lokasi : Kota ${x.city}</p>
         </div>
     `;
 });
