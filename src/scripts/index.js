4fc11cbcc72e62d5636c26ef74d8a26b7e91e0a1
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-21 19:15:10 +0700

    Eslint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,22 @@
-import '../styles/main.css';
-import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css'
+import 'regenerator-runtime'
 import './card'
 
-console.log("Hello")
+const menu = document.getElementById('hamburger')
+const navbar = document.getElementById('drawer')
+const tahun = document.getElementById('tahun')
+const main = document.querySelector('main')
 
-const menu = document.getElementById("hamburger");
-const navbar = document.getElementById("drawer");
-const tahun = document.getElementById("tahun");
-const main = document.querySelector("main");
-
-menu.addEventListener("click", (event) => {
-    navbar.classList.toggle("navbar-open");
-    event.stopPropagation();
+menu.addEventListener('click', (event) => {
+  navbar.classList.toggle('navbar-open')
+  event.stopPropagation()
 })
 
-main.addEventListener("click", (event) => {
-    navbar.classList.remove("navbar-open");
-    event.stopPropagation();
+main.addEventListener('click', (event) => {
+  navbar.classList.remove('navbar-open')
+  event.stopPropagation()
 })
 
-let year = new Date().getFullYear();
+const year = new Date().getFullYear()
 
-tahun.innerHTML = year;
-
+tahun.innerHTML = year
