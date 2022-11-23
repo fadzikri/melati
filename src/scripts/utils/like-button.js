3f55d798a1a8de43b97c99b21310241812f56910
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-23 22:42:57 +0700

    A

--- src/scripts/utils/like-button.js
+++ src/scripts/utils/like-button.js
@@ -4,20 +4,15 @@ const likeButton = async (data) => {
   const like = document.getElementById("like");
   const love = document.getElementsByClassName("fa")[0];
 
-  if (await operationDb.getRestaurant(data.id)) {
-    console.log("a");
-    return like.classList.remove("fa-heart-o");
-  }
-
   like.addEventListener("click", async (e) => {
     e.stopPropagation();
 
     love.classList.toggle("fa-heart-o");
 
     if (love.classList.contains("fa-heart-o")) {
-      operationDb.delRestaurant(data.id);
+      await operationDb.delRestaurant(data.id);
     } else {
-      operationDb.putRestaurant(dataRestaurant(data));
+      await operationDb.putRestaurant(dataRestaurant(data));
     }
   });
 };
