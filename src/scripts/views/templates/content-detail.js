9b8159f916e9e00e315a2aebaa46a4cf7b91cb24
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-12-06 15:54:10 +0700

    Fix Love

--- src/scripts/views/templates/content-detail.js
+++ src/scripts/views/templates/content-detail.js
@@ -19,7 +19,7 @@ class RestaurantDetail {
 
     detailElement.innerHTML = `
       <restaurant-detail-container class="detail">
-        <img src="${this._image}" width="100%" alt="Foto untuk restoran ${this._name}" tabindex="0">
+        <img class="lazy-skeleton lazyload" data-src="${this._image}" width="100%" alt="Foto untuk restoran ${this._name}" tabindex="0">
       </restaurant-detail-container>
       <hr>
       <restaurant-detail-container class="detail">
@@ -47,7 +47,7 @@ class RestaurantDetail {
           <button id="${this._id}" class="input-btn">Kirim</button>
         </form>
         <restaurant-like class="like" id="like" tabindex="0">
-          <svg aria-hidden="true" aria-label="like" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-heart feather-heart-o feather-heart-x"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+          <svg aria-hidden="true" aria-label="like" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-heart feather-heart-o"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
         </restaurant-like>
       </restaurant-detail-container>
     `;
