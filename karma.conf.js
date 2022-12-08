e3a17f9eb64bd3cc94a9bf51afbb0f2b6e47de2e
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2022-12-08 09:48:44 +0700

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ["ChromeHeadless"],
-    singleRun: false,
+    singleRun: true,
     concurrency: Infinity,
     customLaunchers: {
       ChromeHeadless: {
